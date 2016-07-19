75ae00f57f9d60315eaa389a83768b040e6f4262
purpleice9765 <purpleice9765@msn.com>
2016-07-19 21:43:17 +0800

    admin add

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   root 'events#index'
 
   resources :events
+
+  namespace :admin do
+    resources :users
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
