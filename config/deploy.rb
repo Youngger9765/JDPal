c4b2ec24cb6e16ec5aa2efa7582ff23ca6c401a3
youngger9765 <purpleice9765@msn.com>
2016-10-12 09:28:50 +0800

    cap insatall

---
+++ config/deploy.rb
@@ -0,0 +1,45 @@
+`ssh-add`
+# config valid only for current version of Capistrano
+lock '3.6.1'
+
+set :application, 'jdpal'
+set :repo_url, 'git@github.com:Youngger9765/JDPal.git'
+
+
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/deploy/jdpal'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+set :linked_files, fetch(:linked_files, []).push( 'config/database.yml',
+                                                  'config/secrets.yml',
+                                                  'config/facebook.yml')
+
+
+# Default value for linked_dirs is []
+# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+set :passenger_restart_with_touch, true
